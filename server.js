acec71b00bd9c2eef0c433d2ad22ceb25ecbe240
amanuel-asmare <amanuelasmare18@gmail.com>
2025-06-19 21:22:41 +0900

    git update  backend

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const cors = require('cors');
 const path = require('path');
 const signinRoute = require('./routes/signinroute');
 
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+console.log('MONGODB_URI:', process.env.MONGODB_URI ? 'Set' : 'Undefined');
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
+
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+
 
 // Serve uploaded files statically (optional, for backward compatibility)
 app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
